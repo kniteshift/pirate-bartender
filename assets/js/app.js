a314f028c1164249308bcdd27b170b3521c04e0f
alan <alanleunglee@gmail.com>
2016-10-25 16:23:14 -0700

    completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,34 @@
 
-var questions = {
-		bitter:'Lubber of the bitters?',
-		fruity:'Ye enjoy the fruits?',
-		sweets:'Oi! taste for ye sweets?',
-		strong:'Care for the strong?',
-		salty:'Do we have a taste for the salt waters?',
-		};
+var questions = 
+		[{
+			type: 'bitter',
+			question: 'Lubber of the bitters?',
+			ingredient: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel']
+		},
+		{
+			type: 'fruity',
+			question:'Ye enjoy the fruits?',
+			ingredient: ['Slice of orange', 'dash of cassis', 'cherry on top']
+		},
+		{
+			type: 'sweet',
+			question:'Oi! taste for ye sweets?',
+			ingredient: ['Sugar cube', 'spoonful of honey', 'splash of cola']
+		},
+		{
+			type: 'strong',
+			question:'Care for the strong?',
+			ingredient: ['Glug of rum', 'slug of whisky', 'splash of gin'],
+		},
+		{
+			type: 'salty',
+			question:'Do we have a taste for the salt waters?',
+			ingredient: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon']
+		}];
 
 var bartend = function(ingredient){
 	this.ingredient = ingredient;
 }
-
-var strong = new bartend(['Glug of rum', 'slug of whisky', 'splash of gin']);
-var salty = new bartend(['Olive on a stick', 'salt-dusted rim', 'rasher of bacon']);
-var bitter = new bartend(['Shake of bitters', 'splash of tonic', 'twist of lemon peel']);
-var sweet = new bartend(['Sugar cube', 'spoonful of honey', 'splash of cola']);
-var fruity = new bartend(['Slice of orange', 'dash of cassis', 'cherry on top']);
-
-var menu = [strong, salty, bitter, sweet, fruity];
-console.log(menu);
 
 
 var random = function() {
@@ -26,36 +36,44 @@ var random = function() {
 	return number;
 };
 
+var questiontypes = [];
+
+function store() {
+	for (var i = 0; i < questions.length; i++) {
+		questiontypes.push(questions[i].type);
+	}
+};
+store();
+
 $(function(){
 
-	var val = [];
-	var storage = [];
+	questions.map(function(question){
+		$('#questions').prepend('<input type="checkbox" value=' + question.type + ' id="'+ question.type +'" name="drink">' + question.question + '</input><br>');
+	});
 
-	function appendToHTML() {
-		var html = '';
-		for (var key in questions) {
-			html += '<input type="checkbox" value=' + key + ' id="'+key+'" name="drink">' + questions[key] + '</input><br>';
-		}
-		html += '<input type="submit" value="Submit"></input>'
-		$('#questions').append(html);
-	}
-	appendToHTML();
 
 	$('#questions').submit( function(e){
 		e.preventDefault();
-		$('input[type="checkbox"]:checked').each(function(i) {
-			val[i] = $(this).val();
+		var ingredients = [];
+
+		questions.map(function(question){
+			if($.inArray($('input[type="checkbox"]:checked').val(), questiontypes) > -1){
+				ingredients.push(question.ingredient[0]);
+				console.log("if activated");
+			};
 		});
-		bartend.createDrink(val);
+
+		var drink = '';
+		ingredients.map(function(ingredient, i){
+		
+		if(i !== 0) {
+			drink += " with a ";
+		}
+		
+		drink += ingredient;
 	});
 
-	var questionKeys = Object.keys(questions);
-	
-	bartend.prototype.createDrink = function(tastes){
-		for (var i = 0; i < val.length; i++) {
-			if(val[i] == questionKeys[i]) {
-				questionKeys[i].push(storage);
-			}
-		}
-	}
+		$('.atthebar').html(drink);
+	});
+
 });
